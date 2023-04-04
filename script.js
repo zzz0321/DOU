eb14ee8646460dbe86b823279cdd9b43d7ec4645
zzz0321 <126553861+zzz0321@users.noreply.github.com>
2023-04-04 18:12:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.write("Hello,world!");
+
+function sayHello(){
+  var response = prompt("What is your name?")
+  alert("Hello" + response + ", have a nice day!");
+}
+function add(a,b){
+  return a + b;
+}
+console.log(add(20,30));
+console.log(add(18,17));
+
+function showTopic(){
+  var x = document.getElementById("demo");
+  x.style.fontSize = "25px";
+  x.style.color = "red";
+}
